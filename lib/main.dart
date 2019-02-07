b427230df4752c49632738a1a2350ecbb0e9cd61
Dylan Modell <dylan.modell.promo@gmail.com>
2019-02-07 15:01:31 -0800

    moved portait orientation locking to app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
-import 'package:flutter/services.dart';
 
-void main() {
-  //Lock portrait orientation
-  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-  //   .then((_) {
-  //     runApp(new App());
-  //   });
-  runApp(App());
-}
+void main() => runApp(App());
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/harmonic_circles.dart';
 import 'screens/build_your_own_lissajous.dart';
+import 'package:flutter/services.dart';
+
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Lock portrait orientation
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
